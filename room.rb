64f95169c13befc5926b5db3c4c71baf81aa9bb7
Pim <pimgroeneveld92@gmail.com>
2018-07-28 14:33:27 +0100

    now including regular

--- room.rb
+++ room.rb
@@ -10,4 +10,22 @@ class Room
 
   end
 
+  def check_if_empty()
+    return @guest_in_room.count()
+  end
+
+  def check_if_no_song()
+    return @song_in_room.count()
+  end
+
+  def check_in_guest(guest)
+    return @guest_in_room << guest
+  end
+
+  def check_out_guest(guest)
+    check_in_guest(guest)
+    @guest_in_room.delete(guest)
+    return check_if_empty
+  end
+
 end
